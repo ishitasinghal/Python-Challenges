fdfe86b74ea3a8cfcad090bedbac4fe6c4bc443f
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-07-22 12:34:43 +0530

    commit 4

---
+++ even-odd-string.py
@@ -0,0 +1,12 @@
+# t = int(input("Enter number of test cases"))
+# for i in range(t):
+#     string = input()
+# print(string)
+
+# for i in range(int(input())):
+#     s=input()
+# print(*["".join(s[::2]),"".join(s[1::2])])
+
+for i in range(int(input())):
+    s = input()
+    print(s[::2], s[1::2])
