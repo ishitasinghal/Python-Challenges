5a5c971c3766155c8a69b145f950aa23b9f73fb3
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-07-28 13:05:22 +0530

    committed

--- hour-glass-2D.py
+++ hour-glass-2D.py
@@ -1,28 +1,33 @@
-import numpy as np
-# R = int(input("Enter the number of rows:"))
-# C = int(input("Enter the number of columns:"))
-# mat = [[int(input()) for x in range (C)] for y in range(R)]
-# print(mat)
+#!/bin/python3
 
-mat
-
-
+import math
+import os
+import random
+import re
+import sys
 
 
 
+if __name__ == '__main__':
+    arr = []
 
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
 
-
-
-# int sum[] = new int[16];
-#     int h = 0;
-#     for (int i = 0; i < 4; i++) {
-#         for (int j = 0; j < 4; j++) {
-#             sum[h] = arr[i][j] + arr[i][j+1] + arr[i][j+2]
-#                     + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1]
-#                     + arr[i+2][j+2];
-#             h++;
-#         }
-#     }
-#     Arrays.sort(sum);
-#     System.out.println(sum[15]);
+    a = 0
+    x = True
+    for i in range(6):
+        #arr.append(list(input().split())[:6])
+        n = len(arr[i])
+        while n < 6 :
+            arr[i].append("0")
+            n = n + 1
+    for i in range(4) :
+        for j in range(4) :
+            s = int(arr[i][j]) + int(arr[i][j+1]) + int(arr[i][j+2]) + int(arr[i+1][j+1]) +             int(arr[i+2][j]) + int(arr[i+2][j+1]) + int(arr[i+2][j+2])
+            if x :
+                a = s
+                x = False
+            if s > a :
+                a = s
+    print(a)
