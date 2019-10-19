937008d8e496efde5a3c94bb9020acd888369cb7
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-19 19:22:50 +0530

    Just the problem statement.

---
+++ Zipped_challenge.py
@@ -0,0 +1,10 @@
+#making use of the zipped iterable function in Python.
+#Question:
+#Print the averages of all students on separate lines.
+#The averages must be correct up to 1 decimal place.
+#Input Format
+#The first line contains N and X separated by a space.
+#The next X lines contains the space separated marks obtained by students in a particular subject.
+
+#Code 
+
