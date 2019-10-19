c22a43a65db8492a913dc26bf48922bf169dacaa
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-19 19:39:53 +0530

    Committing problem statement

---
+++ AnyAll.py
@@ -0,0 +1,7 @@
+#this basic code makes use of the python's built-in any() and all() function.
+#Problem statement
+#Given a space separated list of integers. 
+#If all the integers are positive, then check if any integer is a palindromic integer.
+#Condition 1:
+#All the integers in the list are positive.
+#Condition 2: any integer is a palindromic integer.
