51d02f11c8ecfd9cf633028b1c6d9cc2f820b3fa
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-19 19:52:46 +0530

    Tested Code committed !

--- AnyAll.py
+++ AnyAll.py
@@ -5,3 +5,8 @@
 #Condition 1:
 #All the integers in the list are positive.
 #Condition 2: any integer is a palindromic integer.
+
+#Code
+
+n,nl = int(input()),input().split()
+print(all([int(i)>0 for i in nl]) and any([j == j[::-1] for j in nl]))
