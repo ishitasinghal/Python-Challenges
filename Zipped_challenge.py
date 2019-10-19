df8fefe75a263dcc68fb15143695418f2bb43aba
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-19 19:33:00 +0530

    Committing the code!

--- Zipped_challenge.py
+++ Zipped_challenge.py
@@ -7,4 +7,11 @@
 #The next X lines contains the space separated marks obtained by students in a particular subject.
 
 #Code 
+n, x = map(int, input().split())
+res = []
+for _ in range(x):
+    res.append( map(float, input().split())) 
+for i in zip(*res): 
+    print( sum(i)/len(i) )
+    
 
