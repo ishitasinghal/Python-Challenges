6075ef11bd123a030dd66cdadd42b9aa499b96c2
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-21 17:01:32 +0530

    Committed code(untested)

---
+++ Reduce.py
@@ -0,0 +1,19 @@
+#This program shows the built-in function in python.
+#The reduce() function applies a function of two arguments
+#cumulatively on a list of objects in succession from left 
+#to right to reduce it to one value. 
+
+#Code
+from fractions import Fraction
+from functools import reduce
+
+def product(fracs):
+    t = reduce(lambda a,b: a*b,fracs)
+    return t.numerator, t.denominator
+
+if __name__ == '__main__':
+    fracs = []
+    for _ in range(int(input())):
+        fracs.append(Fraction(*map(int, input().split())))
+    result = product(fracs)
+    print(*result)
