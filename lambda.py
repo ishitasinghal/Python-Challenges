d28f7e0a30e99e7296bf202f0be0ac45d200357c
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-23 13:32:58 +0530

    Adding function !

--- lambda.py
+++ lambda.py
@@ -2,3 +2,11 @@
 #it is a function that has only one line in its body. 
 
 #A list on a single line containing the cubes of the first N fibonacci numbers.
+cube = lambda x: pow(x,3)
+def fibonacci(n):
+     res = [0,1]
+     for i in range(2,n):
+         res.append(res[i-2] + res[i-1])
+     return(res[0:n])
+  
+#https://www.hackerrank.com/challenges/map-and-lambda-expression/problem
