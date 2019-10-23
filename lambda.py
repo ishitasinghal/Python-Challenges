47b683eff6846c877ac0a3fd4b4e33bf09c407ab
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-23 13:25:52 +0530

    Committing problem statement !

---
+++ lambda.py
@@ -0,0 +1,4 @@
+#Lambda is a single expression anonymous function often used as an inline function. 
+#it is a function that has only one line in its body. 
+
+#A list on a single line containing the cubes of the first N fibonacci numbers.
