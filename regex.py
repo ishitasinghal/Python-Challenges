d3b26d9cd7f5aa07e9b5b58867d64da23ed63bfa
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-27 21:13:16 +0530

    Committing code !

--- regex.py
+++ regex.py
@@ -1,3 +1,11 @@
 #Problem Statement
 #Given a string consisting only of digits 0-9, commas ,, and dots .
 #split it !
+
+#CODE
+
+regex_pattern = r",|\."	
+
+import re
+print("\n".join(re.split(regex_pattern, input())))
+
