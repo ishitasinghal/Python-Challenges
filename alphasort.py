3657691b226085c82ab9739e9cf229b4f5378c69
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-28 22:48:02 +0530

    Few lines of code !

--- alphasort.py
+++ alphasort.py
@@ -4,3 +4,7 @@
 #All sorted lowercase letters are ahead of uppercase letters.
 #All sorted uppercase letters are ahead of digits.
 #All sorted odd digits are ahead of sorted even digits.
+
+n = ''.join(sorted(input()))
+print(n)
+
