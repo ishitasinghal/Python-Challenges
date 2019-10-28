9de0bcb5806c89b603de5e47e94134ef7e3a1568
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-10-28 22:41:28 +0530

    Committing the problem statement !

---
+++ alphasort.py
@@ -0,0 +1,6 @@
+#Problem statement
+
+# sort the string in the following manner:
+#All sorted lowercase letters are ahead of uppercase letters.
+#All sorted uppercase letters are ahead of digits.
+#All sorted odd digits are ahead of sorted even digits.
