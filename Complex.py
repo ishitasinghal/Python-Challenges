f4084a7ebc41bdfc59d0c4c75a3b8a63c6c9f4df
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-02 16:38:10 +0530

    Really complex code though !

--- Complex.py
+++ Complex.py
@@ -3,26 +3,48 @@
 #One line of input: The real and imaginary part of a number separated by a space.
 #Output: Print the result of their addition, subtraction, multiplication, division and modulus operations.
 
+import math
+
 class Complex(object):
     def __init__(self, real, imaginary):
-        self.real=real
-        self.imaginary = imaginary
-        
+        self.real,self.imaginary=real,imaginary
     def __add__(self, no):
-        return(complex(self.real+no.real, self.imaginary+no.imaginary))
-        
+        temp=Complex(0,0)
+        temp.real,temp.imaginary=self.real+no.real,self.imaginary+no.imaginary
+        return temp
     def __sub__(self, no):
-        return(complex(self.real-no.real, self.imaginary-no.imaginary))
-        
+        temp=Complex(0,0)
+        temp.real,temp.imaginary=self.real-no.real,self.imaginary-no.imaginary
+        return temp
     def __mul__(self, no):
-        return complex(self.real*no.real-self.imaginary*no.imaginary, self.real*no.imaginary+self.imaginary*no.real)
-
+        temp=Complex(0,0)
+        temp.real,temp.imaginary=(self.real*no.real-self.imaginary*no.imaginary),(self.real*no.imaginary+self.imaginary*no.real)
+        return temp
     def __truediv__(self, no):
-        try: 
-            return self.__mul__(complex(no.real, -1*no.imaginary)).__mul__(complex(1.0/(no.mod().real)**2, 0))
-        except ZeroDivisionError as e:
-            print(e)
-            return None
-
+        temp=Complex(0,0)
+        temp.real,temp.imaginary=(self.real*no.real+self.imaginary*no.imaginary)/(no.real**2+no.imaginary**2),(self.imaginary*no.real-self.real*no.imaginary)/(no.real**2+no.imaginary**2)
+        return temp
     def mod(self):
-        return complex(pow(self.real**2+self.imaginary**2, 0.5), 0)
+        temp=Complex(0,0)
+        temp.real,temp.imaginary=math.sqrt(self.real**2+self.imaginary**2),0
+        return temp
+    def __str__(self):
+        if self.imaginary == 0:
+            result = "%.2f+0.00i" % (self.real)
+        elif self.real == 0:
+            if self.imaginary >= 0:
+                result = "0.00+%.2fi" % (self.imaginary)
+            else:
+                result = "0.00-%.2fi" % (abs(self.imaginary))
+        elif self.imaginary > 0:
+            result = "%.2f+%.2fi" % (self.real, self.imaginary)
+        else:
+            result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
+        return result
+
+if __name__ == '__main__':
+    c = map(float, input().split())
+    d = map(float, input().split())
+    x = Complex(*c)
+    y = Complex(*d)
+    print(*map(str, [x+y, x-y, x*y, x/y, x.mod(), y.mod()]), sep='\n')
