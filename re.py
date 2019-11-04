beafa205e558c6112fdca235dbea3990ab29ea5c
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-04 20:30:15 +0530

    Final Code updated!

--- re.py
+++ re.py
@@ -1,1 +1,8 @@
 #A list is to be printed containing only valid email addresses in lexicographical order.
+
+#Code
+#Just a one liner using regular expressions.
+
+def fun(s):
+    return(bool(re.findall(r'^[\w_-]+@[a-zA-Z0-9]+\.[\w]{0,3}$', s)))
+  
