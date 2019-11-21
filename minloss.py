591e8ea3ff9db9b2cc150ba4e91923e92eef6cc9
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-21 14:34:05 +0530

    Version 2

--- minloss.py
+++ minloss.py
@@ -18,3 +18,22 @@ def minimumLoss(price):
 
 price=[20,7,8,2,5]
 minimumLoss(price)
+
+
+
+def minimumLoss(price):
+    list1 =[]
+    j=0
+    i=0
+    while(i<=len(price)):
+        for j in range(len(price)-1):
+            for k in range(j+1,len(price)):
+                list1.append(price[j]-price[k])
+            j=j+1
+            #print(list1)
+        i=i+1
+        price=price[i:]
+        list2=list1
+    #print(len(list2))    
+    list2 = [x for x in list2 if x >= 0 ]
+    return(min(list2))
