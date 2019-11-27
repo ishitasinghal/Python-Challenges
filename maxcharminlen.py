01c841658efd79ccaf67eb408b08f5addf590d9d
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-27 22:51:19 +0530

    Initial commit

---
+++ maxcharminlen.py
@@ -0,0 +1,14 @@
+NO_OF_CHARS = 256
+def dischar(str,n):
+    count[0]*NO_OF_CHARS
+    for i in range(n):
+        count[ord(str[i])]+=1
+    maxd=0
+    for j in range(NO_OF_CHARS):
+        if(count[i[!=0):
+                 maxd+=1
+    return maxd
+
+def subs(str):
+    n=len(str)
+        
