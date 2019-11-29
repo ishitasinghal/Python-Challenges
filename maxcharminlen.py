804eb4bf7f8fdaba1043d4cef77872102c849111
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-11-29 22:40:49 +0530

    Update maxcharminlen.py

--- maxcharminlen.py
+++ maxcharminlen.py
@@ -1,26 +1,27 @@
 NO_OF_CHARS = 256
-def dischar(str,n):
-    count[0]*NO_OF_CHARS
-    for i in range(n):
-        count[ord(str[i])]+=1
-    maxd=0
-    for j in range(NO_OF_CHARS):
-        if(count[i]!=0):
-                 maxd+=1
-    return maxd
+def diffchars(str, n): 
+	count = [0] * NO_OF_CHARS 
+	for i in range(n): 
+		count[ord(str[i])] += 1
+	max_distinct = 0
+	for i in range(NO_OF_CHARS): 
+		if (count[i] != 0): 
+			max_distinct += 1	
+	return max_distinct 
 
-def subs(str):
-    n=len(str)
-    maxdis=dischar(str,n)
-    minl=n
-    for i in range(n):
-        for j in range(n):
-           subs=str[i:j]
-           subs_length=len(subs)
-           sub_dist_char=max_distinct_char(subs,subs_length)
-           if(subs_length<minl and max_distinct==sub_dist_char):
-                minl=subslength
-    return minl
-str="abcda"
-l=subs(str)
-print(l)
+def smallesteSubstr_maxDistictChar(str): 
+	n = len(str)
+	max_distinct = diffchars(str, n) 
+	minl = n 
+	for i in range(n): 
+		for j in range(n): 
+			subs = str[i:j] 
+			subs_length = len(subs) 
+			sub_distinct_char = diffchars(subs, subs_length) 
+			if (subs_length < minl and max_distinct == sub_distinct_char): 
+				minl = subs_length 
+	return minl 
+str = "abcda"
+
+l = smallesteSubstr_maxDistictChar(str); 
+print(l) 
