32afa58ff535eb4fdeb2b5816ba8b6c902a0e11b
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-12-01 16:12:27 +0530

    Update chaoticqueue.py

--- chaoticqueue.py
+++ chaoticqueue.py
@@ -6,3 +6,31 @@
 
 #Link to the complete problem statement-
 #https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+#CODE
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumBribes function below.
+def minimumBribes(q):
+    flist=[]
+    nq = sort(q)
+    for i in range(len(q)):
+        flist.append(nq[i]-q[i])
+    print(flist)
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        q = list(map(int, input().rstrip().split()))
+
+        minimumBribes(q)
