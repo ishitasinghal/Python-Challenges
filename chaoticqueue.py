2e8e049678e672ba11d14f4cc2c4a4528e20d46c
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-12-01 16:05:58 +0530

    just the problem statement !

---
+++ chaoticqueue.py
@@ -0,0 +1,8 @@
+#Handle the chaotic queue
+#Any person in the queue can bribe the person directly in front of them to swap positions. 
+#If two people swap positions, they still wear the same sticker denoting their original places in line. 
+#One person can bribe at most two others. 
+#decide you must know the minimum number of bribes that took place to get the queue into its current state!
+
+#Link to the complete problem statement-
+#https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
