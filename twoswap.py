e27129e535dac43a3e019491cee41121d2046be3
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-12-06 22:54:36 +0530

    Commit 1

--- twoswap.py
+++ twoswap.py
@@ -1,20 +1,40 @@
 #You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] 
 #without any duplicates. You are allowed to swap any two elements. 
 #You need to find the minimum number of swaps required to sort the array in ascending order.
-ef Repeat(x): 
 
-    _size = len(x) 
 
-    repeated = [] 
+#CODE
 
-    for i in range(_size): 
 
-        k = i + 1
+#!/bin/python3
 
-        for j in range(k, _size): 
+import math
+import os
+import random
+import re
+import sys
 
-            if x[i] == x[j] and x[i] not in repeated: 
+# Complete the minimumSwaps function below.
+def minimumSwaps(arr):
+    swaps=0
+    for i in range(len(arr)):
+        for j in range(len(arr)-1):
+            if(arr[j]>arr[j+1]):
+                temp = arr[j]
+                arr[j]=arr[j+1]
+                arr[j+1]=temp
+                swaps=swaps+1
+    return(swaps)
 
-                repeated.append(x[i]) 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    return repeated 
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = minimumSwaps(arr)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
