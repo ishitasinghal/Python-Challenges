5092c73158b2aff250511533a87ed8be5e2a5be6
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-12-07 23:54:52 +0530

    Update maxcharminlen.py

--- maxcharminlen.py
+++ maxcharminlen.py
@@ -2,3 +2,9 @@ sample = 'abcda'
 subs = [sample[i: j] for i in range(len(sample)) for j in range(i + 1, len(sample) + 1)] 
 max_dist = len(set(sample))
 print(max_dist)
+sample = 'abcda'
+subs = [sample[i: j] for i in range(len(sample)) for j in range(i + 1, len(sample) + 1)] 
+max_dist = len(set(sample))
+for k in subs:
+    if(len(set(k))<=max_dist):
+        print(k)
