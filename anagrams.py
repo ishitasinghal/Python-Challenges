603317c9f33b55c3a1dc8902c40482104fa220ba
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-12-08 23:54:12 +0530

    Update required

--- anagrams.py
+++ anagrams.py
@@ -3,3 +3,15 @@
 #Any characters can be deleted from either of the strings.
 #we can delete from string and from string  
 #so that both remaining strings are which are anagrams.
+
+#Code
+
+s1 = "fcrxzwscanmligyxyvym"
+s2 = "jxwtrhvujlmrpdoqbisbwhmgpmeoke"
+ns=""
+for i in s1:
+    if(i in s2):
+        ns = ns+i
+nlen = len(ns)
+final = (len(s1)-len(ns)+len(s2)-len(ns))
+print(final)
