84bf2831309bc3d5f0f8cd701451a0a145d7f2e9
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-12-08 20:52:54 +0530

    Just the problem statement

---
+++ anagrams.py
@@ -0,0 +1,5 @@
+#Given two strings a, and b, that may or may not be of the same length, determine 
+#the minimum number of character deletions required to make  and  anagrams. 
+#Any characters can be deleted from either of the strings.
+#we can delete from string and from string  
+#so that both remaining strings are which are anagrams.
