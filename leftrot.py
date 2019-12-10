97e41860ea31798774c35bf2f9d62fa97efcef70
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-12-10 23:05:58 +0530

    Update leftrot.py

--- leftrot.py
+++ leftrot.py
@@ -1,11 +1,12 @@
 def rotLeft(a, d):
     while(d>0):
-        temp = a[len(a)-1]
-        a[len(a)-1]=a[0]
-        for i in range(0,len(a)-1):
+        for i in range(0,len(a)-2):
+            temp = a[0]
             a[i]=a[i+1]
-        print(a)
+        a[len(a)-1]=temp
         d=d-1
+    print(a)
+        
 
 a = [1,2,3,4,5]
 d=2
