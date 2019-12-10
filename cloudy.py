ef1c59f007228786a1a3fb2656afb9af06d4af7e
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-12-10 23:48:58 +0530

    initial commit!

---
+++ cloudy.py
@@ -0,0 +1,9 @@
+c = [0, 0, 0, 1, 0, 0]
+safepath = [i for i in range(len(c)) if(c[i]==0)]
+print(safepath)
+possible = [safepath[j] for j in range(len(safepath)-1) if(safepath[j+1]-safepath[j])<=2]
+p2=[]
+for i in range(len(possible)):
+    if(possible[i+2]-possible[i]==2):
+        p2.append
+print(possible)
