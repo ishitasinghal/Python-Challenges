02d429dfaaa2cf4b86045b657c1914a5dea18202
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-12-10 21:32:54 +0530

    Update anagrams.py

--- anagrams.py
+++ anagrams.py
@@ -20,20 +20,6 @@ print(final)
 
 
 
-
-
-def check(s1, s2): 
-
-      
-
-    # the sorted strings are checked  
-
-    if(sorted(s1)== sorted(s2)): 
-
-        print("The strings are anagrams.")  
-
-    else: 
-
-        print("The strings aren't anagrams.")          
+        
 
           
