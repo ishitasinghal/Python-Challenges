abf95e75f78476c7db8d47389c3aa78316f39836
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-12-13 16:48:43 +0000

    Just a comment added!

--- cloudy.py
+++ cloudy.py
@@ -1,5 +1,5 @@
 c = [0, 0, 0, 1, 0, 0]
-safepath = [i for i in range(len(c)) if(c[i]==0)]
+safepath = [i for i in range(len(c)) if(c[i]==0)] #making a list of the possible safepaths
 possible = [safepath[j] for j in range(len(safepath)-1) if(safepath[j+1]-safepath[j])<=2]
 p2=[]
 for i in range(len(possible)):
