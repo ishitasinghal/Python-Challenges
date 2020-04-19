373758b92ede27cbbd50404ce8b273d34b240d5e
Ishita Singhal <ishitasinghal08@gmail.com>
2020-04-20 00:00:28 +0530

    All test-cases running.

--- list_str_to_int.py
+++ list_str_to_int.py
@@ -1,23 +1,30 @@
-Source: "coderbyte.com/editor/Find%20Intersection:Python3"
+# Source: "coderbyte.com/editor/Find%20Intersection:Python3"
 
 
-Have the function FindIntersection(strArr) read the array of strings stored in strArr which will contain 2 elements: the first element will represent a list of comma-separated numbers sorted in ascending order, the second element will represent a second list of comma-separated numbers (also sorted). Your goal is to return a comma-separated string containing the numbers that occur in elements of strArr in sorted order. If there is no intersection, return the string false.
-Examples
-Input: ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
-Output: 1,4,13
-Input: ["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"]
-Output: 1,9,10
+# Have the function FindIntersection(strArr) read the array of strings stored in strArr which will contain 2 elements: the first element will represent a list of comma-separated numbers sorted in ascending order, the second element will represent a second list of comma-separated numbers (also sorted). Your goal is to return a comma-separated string containing the numbers that occur in elements of strArr in sorted order. If there is no intersection, return the string false.
+# Examples
+# Input: ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
+# Output: 1,4,13
+# Input: ["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"]
+# Output: 1,9,10
 
  
 
-CODE
+# CODE
 
-a = ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
-li = []
-a1 = a[0].split(',')
-a2 = a[1].slit(',')
-arr1 = [int(a) for a in a1]
-arr2 = [int(a) for a in a2]
-for i in arr1:
-    if i in arr2:
-        li.append(i)
+def FindIntersection(strArr):
+  li = []
+  a1 = strArr[0].split(',')
+  a2 = strArr[1].split(',')
+  arr1 = [int(a) for a in a1]
+  arr2 = [int(a) for a in a2]
+  for i in arr1:
+      if i in arr2:
+          li.append(i)
+
+  nli = [str(a) for a in li]
+  print("," . join(nli))
+
+
+# keep this function call here 
+FindIntersection(input())
