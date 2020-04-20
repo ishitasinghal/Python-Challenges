34b71c5c15cd252aee0edcbc430fda9c0d793b5e
Ishita Singhal <ishitasinghal08@gmail.com>
2020-04-21 00:00:43 +0530

    Update ques_marks.py

--- ques_marks.py
+++ ques_marks.py
@@ -16,14 +16,14 @@
 
 def QuestionsMarks(str):
   a=11
-    b='false'
+    status='false'
     c=0
     for i in str:
       if i.isdigit():
         if int(i)+a==10:
           if c!=3:
             return 'false'
-          b='true'
+          status='true'
         c=0
         a=int(i)
       elif i=='?':
