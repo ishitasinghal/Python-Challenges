a8ee6c7ef2fdc5870552f3c2cce08bad2b8f6343
Ishita Singhal <ishitasinghal08@gmail.com>
2020-04-22 00:25:19 +0530

    Update letters.py

--- letters.py
+++ letters.py
@@ -1,11 +1,25 @@
-Source : "https://www.coderbyte.com/editor/Letter%20Changes:Python3"
+# Source : "https://www.coderbyte.com/editor/Letter%20Changes:Python3"
 
-Problem Statement:
-Have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.
-Examples
-Input: "hello*3"
-Output: Ifmmp*3
-Input: "fun times!"
-Output: gvO Ujnft!
+# Problem Statement:
+# Have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.
+# Examples
+# Input: "hello*3"
+# Output: Ifmmp*3
+# Input: "fun times!"
+# Output: gvO Ujnft!
 
-CODE
+# CODE
+
+def LetterChanges(str):
+  nstr = ""
+  for i in str:
+    if(i.isalpha()):
+      nstr = nstr+chr(ord(i) + 1) 
+    elif(i.isspace()):
+      nstr = nstr+i
+
+  
+  return nstr
+
+# keep this function call here 
+print(LetterChanges(input()))
