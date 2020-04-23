49de09fbd6ecc2fe54f1b99ec10a48b2bffe9522
Ishita Singhal <ishitasinghal08@gmail.com>
2020-04-23 23:46:01 +0530

    Problem Statement.

---
+++ binary_reversal.py
@@ -0,0 +1,9 @@
+Source:  "https://www.coderbyte.com/editor/Binary%20Reversal:Python3"
+
+Problem Statement:
+Have the function BinaryReversal(str) take the str parameter being passed, which will be a positive integer, take its binary representation (padded to the nearest N * 8 bits), reverse that string of bits, and then finally return the new reversed string in decimal form. For example: if str is "47" then the binary version of this integer is 101111 but we pad it to be 00101111. Your program should reverse this binary string which then becomes: 11110100 and then finally return the decimal version of this string, which is 244.
+Examples
+Input: "213"
+Output: 171
+Input: "4567"
+Output: 60296
