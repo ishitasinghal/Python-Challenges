baa7ecffda21d78645a5b4e16ea65fcca3ec622a
Ishita Singhal <ishitasinghal08@gmail.com>
2020-04-26 00:18:04 +0530

    Problem Statement

---
+++ longest.py
@@ -0,0 +1,10 @@
+Source: "https://www.coderbyte.com/editor/Longest%20Word:Python3"
+
+Problem Statement:
+Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
+Examples
+Input: "fun&!! time"
+Output: time
+Input: "I love dogs"
+Output: love
+
