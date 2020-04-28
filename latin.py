f0c9bdc690870aa2664db52192252ff956156836
Ishita Singhal <ishitasinghal08@gmail.com>
2020-04-28 13:27:12 +0530

    Update latin.py

--- latin.py
+++ latin.py
@@ -1,13 +1,16 @@
-Source: "https://codeforces.com/problemset/problem/1335/B"
+# Source: "https://codeforces.com/problemset/problem/1335/B"
 
-Problem Statement:
-You are given three positive integers n, a and b. You have to construct a 
-string s of length n consisting of lowercase Latin letters such that each 
-substring of length a has exactly b distinct letters.
+# Problem Statement:
+# You are given three positive integers n, a and b. You have to construct a 
+# string s of length n consisting of lowercase Latin letters such that each 
+# substring of length a has exactly b distinct letters.
 
-CODE:
+# CODE:
 
 import random
 import string
 n, a, b = input().split()
-print(random.choice(string.ascii_letters))
+string1 = ""
+while(len(string1)<int(a)):
+    string1 = string1 + (random.choice(string.ascii_letters))
+print(string1)
