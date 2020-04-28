1de7201a1c4bec5f76d5956c80367ee4b431a147
Ishita Singhal <ishitasinghal08@gmail.com>
2020-04-28 23:06:25 +0530

    Update latin.py

--- latin.py
+++ latin.py
@@ -11,14 +11,18 @@
 
 import random
 import string
-n, a, b = input().split()
-string1 = ""
-while(len(string1)<int(b)):
-    string1 = string1 + (random.choice(string.ascii_lowercase))
-for i in range(int(a)-int(b)):
-    string1=string1+string1[i]
+testcases = int(input())
+for i in range(testcases):
+    n, a, b = input().split()
+    string1 = ""
+    while(len(string1)<int(b)):
+        string1 = string1 + (random.choice(string.ascii_lowercase))
 
-for i in range(int(n)-int(a)):
-    string1=string1+string1[i]
+    for i in range(int(a)-int(b)):
+        string1=string1+string1[i]
 
-print(string1)
+    for i in range(int(n)-int(a)):
+        string1=string1+string1[i]
+
+    print(string1)
+
