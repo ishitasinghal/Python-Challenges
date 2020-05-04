f3fad7586ceee1b9a74fb84bbb01c720791b3c8e
Ishita Singhal <ishitasinghal08@gmail.com>
2020-05-04 23:47:27 +0530

    Problem Statement

---
+++ reverse.py
@@ -0,0 +1,12 @@
+Source: "https://www.coderbyte.com/editor/First%20Reverse:Python3"
+
+Problem Statement:
+
+Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+Examples
+Input: "coderbyte"
+Output: etybredoc
+Input: "I Love Code"
+Output: edoC evoL I
+
+CODE
