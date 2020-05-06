8c4d121c911ddef939c61358a1c0def6692728be
Ishita Singhal <ishitasinghal08@gmail.com>
2020-05-06 23:02:02 +0530

    Update regex2.py

--- regex2.py
+++ regex2.py
@@ -1,3 +1,19 @@
-Problem Statement:
-ou are given a string S.
-Your task is to find the indices of the start and end of k string in S .
+# Problem Statement:
+# ou are given a string S.
+# Your task is to find the indices of the start and end of k string in S .
+
+import re
+
+s = input()
+k = input()
+index = 0
+
+if re.search(k, s):
+    while index+len(k) < len(s):
+        m = re.search(k, s[index:]) 
+        
+        print("({0}, {1})".format(index+m.start(), index+m.end()-1)) 
+        
+        index += m.start() 
+else:
+    print((-1, -1))
