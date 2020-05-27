6b645857300d5c87a0a22ebd69a8a5fbc6242fce
Ishita Singhal <ishitasinghal08@gmail.com>
2020-05-27 15:36:19 +0530

    Update mostfrequentword.py

--- mostfrequentword.py
+++ mostfrequentword.py
@@ -1,3 +1,6 @@
 string = "This is a string from which words are to be counted is a this from words words counted"
 list1 = string.split()
-print(list1)
+fre = []
+for i in list1:
+    fre.append(list1.count(i))
+print(str(list(zip(list1, fre))))
