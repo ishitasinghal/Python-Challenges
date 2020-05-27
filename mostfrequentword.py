f0f921e708670c0d2a8be6860eb4e99b92c981fa
Ishita Singhal <ishitasinghal08@gmail.com>
2020-05-27 15:58:15 +0530

    Update mostfrequentword.py

--- mostfrequentword.py
+++ mostfrequentword.py
@@ -11,4 +11,5 @@ from collections import Counter
 string = "This is a string from which words are to be counted is a this from words words counted"
 list1 = string.split()
 countstr = Counter(list1)
-print(countstr.most_common(3))
+##print(countstr.most_common(1)[0])
+res_list = [x for x,_ in countstr.most_common()]
