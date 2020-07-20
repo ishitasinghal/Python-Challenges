2e93e12ca3c4b97a1f2b31fa36efd6a00748db76
Ishita Singhal <ishitasinghal08@gmail.com>
2020-07-20 14:02:05 +0530

    Working

--- solution.py
+++ solution.py
@@ -14,7 +14,11 @@
 
 # CODE
 
-st = "abaaba"
-stlist = [st[i: j] for i in range(len(st)) for j in range(i + 1, len(st) + 1)]
-stset = set(stlist)
-print(stset)
+void t(String s){
+    for(int l=0,i=0,t=s.length(),q=0;l++<t&q<1;i=0)
+        for(String b;i<=t-l;)
+            if(s.indexOf(b=s.substring(i,i++ +l),s.indexOf(b)+1)<0){
+                System.out.println(b);
+                q++;
+            }
+}
