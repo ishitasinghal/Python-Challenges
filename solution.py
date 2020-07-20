91dad4f197715b816c0cbcdbc1e9eb7bf6ddeb41
Ishita Singhal <ishitasinghal08@gmail.com>
2020-07-20 13:40:23 +0530

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,13 @@
+Problem Statement
+Write a function solution that :
+Given a string S of length N, returns the length of the shortest unique
+substring of S, i.e. the length of the shortest word, which ocuurs in S exactly once. 
+Example:
+1. Given S = "abaaba" the function should return 2. The shortest unique substring of S is aa.
+2. Given S="zyzyzyz" the function should return 5. he shortest unique substring of S is "yzyzy"
+
+Note that there are shortest words, like yzy, occurences of which overlap 
+but they still count as mulitple occurences.
+
+3. Given S = "aabbbabaaa" the func should return 3. all substring of size ocuurs in S 
+atleast twice.
