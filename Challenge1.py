894aeec4df206bd85feddabe9554d80b1f767120
Ishita Singhal <ishitasinghal08@gmail.com>
2020-11-25 18:35:08 +0530

    Update Challenge1.py

--- Challenge1.py
+++ Challenge1.py
@@ -2,8 +2,10 @@ ar=[10, 20, 20, 10, 10, 30, 50, 10, 20]
 my_dict = {i:ar.count(i) for i in ar}
 my_list=[]
 a=b=c=0
+
 for value in my_dict:
     my_list.append(my_dict[value])
+    
 for i in my_list:
     if(i%2==1):
         a=a+int(i/2)
